9f38c9f5080d2513d564afcd64731882b57f003f
turner-h <turnerclayton532@gmail.com>
2022-04-06 12:08:45 -0400

    jumped to long mode

--- bootloader/asm/boot.asm
+++ bootloader/asm/boot.asm
@@ -65,26 +65,148 @@ dw 0xaa55
 copy_target:
 bits 32
 
-hello: db 0
+noCPUID:
+	hlt
+
+no_long_mode:	
+	hlt
+
+check_CPUID:
+	pushfd
+	pop eax
+
+	mov ecx, eax
+	xor eax, 1 << 21
+
+	push eax
+	popfd
+	pushfd
+	pop eax
+	
+	push ecx
+	popfd
+
+	xor eax, ecx
+	jz noCPUID
+	ret
 
 boot2:
-	mov esi,hello
-	mov ebx,0xb8000
+	call check_CPUID
+	
+	; check if long mode exists
+	mov eax, 0x80000000
+	cpuid
+	cmp eax, 0x80000001
+	jb no_long_mode
+	mov eax, 0x80000001
+	cpuid
+	test edx, 1 << 29
+	jz no_long_mode
 
-.loop:
-	lodsb
-	or al,al
-	jz halt
-	or eax,0x0100
-	mov word [ebx], ax
-	add ebx,2
-	jmp .loop
+	; set up paging
+	mov edi, 0x1000
+	mov cr3, edi
+	xor eax, eax
+	mov ecx, 4096
+	rep stosd
+	mov edi, cr3
+
+	mov dword [edi], 0x2003
+	add edi, 0x1000
+	mov dword [edi], 0x3003
+	add edi, 0x1000
+	mov dword [edi], 0x4003
+	add edi, 0x1000
+
+	mov ebx, 0x00000003 
+	mov ecx, 512
+
+	.set_entry:
+		mov dword [edi], ebx
+		add ebx, 0x1000
+		add edi, 8
+		loop .set_entry
+
+	mov eax, cr4
+	or eax, 1 << 5
+	mov cr4, eax
+	
+	mov ecx, 0xC0000080
+	rdmsr
+	or eax, 1 << 8
+	wrmsr
+
+	mov eax, cr0
+	or eax, 1 << 31
+	mov cr0, eax
+
+	lgdt [GDT.Pointer]
+	jmp GDT.Code:Realm64
+	hlt
+
+; access bits
+PRESENT equ 1 << 7
+NOT_SYS equ 1 << 4
+EXEC equ 1 << 3
+DC equ 1 << 2
+RW equ 1 << 1
+ACCESSED equ 1 << 0
+
+; flags bits
+GRAN_4K equ 1 << 7
+SZ_32 equ 1 << 6
+LONG_MODE equ 1 << 5
+
+GDT:
+	.Null: equ $ - GDT
+		dq 0
+	.Code: equ $ - GDT
+		dd 0xFFFF
+		db 0 
+		db PRESENT | NOT_SYS | EXEC | RW
+		db GRAN_4K | LONG_MODE | 0xF
+		db 0
+	.Data: equ $ - GDT
+		dd 0xFFFF
+		db 0
+		db PRESENT | NOT_SYS | RW
+		db GRAN_4K | SZ_32 | 0xF
+		db 0
+	.TSS: equ $ - GDT
+		dd 0x00000068
+        dd 0x00CF8900
+	.Pointer:
+		dw $ - GDT - 1
+		dq GDT
+
+bits 64
+hello: db 0
+
+Realm64:
+	cli
+	mov ax, GDT.Data
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+
+	mov esi,hello
+    mov ebx,0xb8000
+	
+	.loop:
+		lodsb
+		or al,al
+		jz halt
+		or eax,0x0F00
+		mov word [ebx], ax
+		add ebx,2
+		jmp .loop
 
 halt:
 	mov esp, kernel_stack_top
     extern _start
     call _start
-    cli
     hlt
 
 section .bss
--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,20 +1,27 @@
 #![no_std]
 #![no_main]
-#![feature(lang_items)]
 
 use core::panic::PanicInfo;
-use core::arch::asm;
-
-static HELLO: &[u8] = b"Hello from Rust!!!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    unsafe { asm!("mov dword ptr [0xb8000], 0x07690748"); }
+    let vga_buffer = 0xb8000 as *mut u8;
+    
+    unsafe {
+        *vga_buffer = b'R';
+        *vga_buffer.offset(1) = 0x0b;
+        *vga_buffer.offset(2) = b'u';
+        *vga_buffer.offset(3) = 0x0b;
+        *vga_buffer.offset(4) = b's';
+        *vga_buffer.offset(5) = 0x0b;
+        *vga_buffer.offset(6) = b't';
+        *vga_buffer.offset(7) = 0x0b;
+    }
 
     loop {}
 }
 
 #[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
